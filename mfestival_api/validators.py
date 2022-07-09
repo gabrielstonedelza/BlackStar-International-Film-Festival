3f033753997ee194e0e2d958e41bdf10c30cc219
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-07-09 07:11:44 +0000

    bsiff api,adjusted file upload validator size

--- mfestival_api/validators.py
+++ mfestival_api/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def validate_trailer_size(value):
     fileSize = value.size
 
-    if fileSize > 31457280:
-        raise ValidationError("File size cannot be above 30MB")
+    if fileSize > 52428800:
+        raise ValidationError("File size cannot be above 50MB")
     else:
         return value
