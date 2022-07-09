df4a411d83c6d9e61a8279387c67bd84a9f700a3
gabrielstonedelza <gabrielstonedelza@gmail.com>
2022-07-09 08:47:19 +0000

    bsiff api,added a default image for poster

--- mfestival_api/validators.py
+++ mfestival_api/validators.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 def validate_trailer_size(value):
     fileSize = value.size
 
-    if fileSize > 52428800:
-        raise ValidationError("File size cannot be above 50MB")
+    if fileSize > 157286400:
+        raise ValidationError("File size cannot be above 150MB")
     else:
         return value
